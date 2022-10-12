42a448cb01183ecbe3b07b53f0bd7dd1c8590862
Samuel Hart <43560556+sfhart33@users.noreply.github.com>
2022-10-12 15:53:56 -0700

    Change app based on notes from henry

--- app.R
+++ app.R
@@ -2,7 +2,6 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 
-#######################
 # Load Data
   gapminder <- read_csv("gapminder_clean.csv") %>%
     as_tibble()
@@ -10,11 +9,25 @@ library(plotly)
   years <- gapminder$Year %>% unique()
   gap_col <- names(gapminder)
   data_type <- gap_col[c(4:18,20)]
-#######################
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-    
+  
+  # https://stackoverflow.com/questions/44324783/dynamically-adjust-height-and-or-width-of-shiny-plotly-output-based-on-window-si
+  tags$head(tags$script('
+                        var dimension = [0, 0];
+                        $(document).on("shiny:connected", function(e) {
+                        dimension[0] = window.innerWidth;
+                        dimension[1] = window.innerHeight;
+                        Shiny.onInputChange("dimension", dimension);
+                        });
+                        $(window).resize(function(e) {
+                        dimension[0] = window.innerWidth;
+                        dimension[1] = window.innerHeight;
+                        Shiny.onInputChange("dimension", dimension);
+                        });
+                        ')),
+  
   # App title ----
   titlePanel("Interactive plotting of the gapminder data set, by Sam Hart"),
   
@@ -58,7 +71,7 @@ ui <- fluidPage(
     mainPanel(
       
       # Output: Histogram ----
-      plotlyOutput(outputId = "gapPlot")
+      plotlyOutput(outputId = "gapPlot", width = "auto")
       
     )
   )
@@ -72,8 +85,11 @@ server <- function(input, output) {
     
     plot1 <- gapminder %>%
       filter(Year == input$year) %>%
+      filter(!is.na(continent)) %>%
       ggplot(aes(x = .data[[input$x_ax]], y = .data[[input$y_ax]], size = pop, color = continent, text = `Country Name`)) +
-      geom_point()
+      geom_point() +
+      ggtitle(paste0("Year = ", input$year))+
+      guides(size=guide_legend(title=NULL))
     
     if(input$logX == "log"){
       plot1 <-  plot1 +
@@ -85,7 +101,7 @@ server <- function(input, output) {
         scale_y_log10()
     }
     
-    ggplotly(plot1)
+    ggplotly(plot1, width = (0.6*as.numeric(input$dimension[1])), height = (0.8*as.numeric(input$dimension[2])))
     
   })
   
